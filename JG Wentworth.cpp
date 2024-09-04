f341f5422d9f5aca82acac8cbede9a5b02bd95a6
JBennett46 <jbennett46@saddleback.edu>
2024-09-04 03:53:05 +0000

    Week One assingments

--- JG Wentworth.cpp
+++
@@ -1,5 +0,0 @@
-#include<iostream>
-using namespace std;
-int main() {
-    cout << "\n\nIt's\nMy\nMoney\n\nAnd\nI\nNeed\nIt\nNow\n\n";
-}
--- hello.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-int main() {
-// this is a note for a human. Comment lines start with // and it tells the compiler to ignore that line
-// "cout" commands stand for console out and always ends with ;
-// a cout command ending in \n will go to the next line
-    cout << "Hello C++ World!";
-}
